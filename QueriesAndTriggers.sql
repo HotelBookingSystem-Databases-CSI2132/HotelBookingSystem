f6d5e2581ba45c5b73179505cbc29ceba589e24c
Steven Li Wing <64179309+sliwi@users.noreply.github.com>
2021-04-03 15:35:04 -0400

    Finished: Trigger 1, Query 1 and 2

---
+++ QueriesAndTriggers.sql
@@ -0,0 +1,43 @@
+-- Point 5: Two Queries and Two Triggers
+
+--Function and Trigger 1:
+
+--Function that updates number of hotels in hotelChain:
+
+CREATE FUNCTION update_number_of_hotels()
+RETURNS TRIGGER AS
+$BODY$
+BEGIN
+
+UPDATE HotelChain  
+SET number_of_hotels = (SELECT count(*)
+FROM Hotel H WHERE H.id = id);
+
+RETURN NEW;
+END
+
+$BODY$ LANGUAGE plpgsql;
+
+
+--Trigger that runs before a hotel instance is deleted. Calls above function.
+
+CREATE TRIGGER number_of_hotels_trigger 
+AFTER UPDATE ON Hotel
+EXECUTE PROCEDURE update_number_of_hotels();
+
+--Function and Trigger 2 (NEEDS TO BE ADDED) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+--Query 1: Finds the minimum price of a hotel that is at least for stars
+
+SELECT MIN(price) AS min_price FROM (SELECT * FROM HotelRoom AS TableA
+JOIN
+(SELECT rating,id FROM Hotel WHERE rating >= 4) AS TableB
+ON TableA.hotel = TableB.id) As TableC
+
+
+--Query 2: Finds the average room price per hotel chains
+
+SELECT AVG(price) avPrice,brand_name FROM BookingInfo BI, Hotel H, HotelChain HC
+WHERE BI.hotel_id = H.id AND H.hotelChain = HC.id
+GROUP BY brand_name
