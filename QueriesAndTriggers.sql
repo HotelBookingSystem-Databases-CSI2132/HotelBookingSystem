a0c0c6ee0d1bd5b07bd4abf897c55d931085d886
NouraldinZ <nzeina99@hotmail.com>
2021-04-03 18:16:17 -0400

    trigger 2

--- QueriesAndTriggers.sql
+++ QueriesAndTriggers.sql
@@ -9,7 +9,7 @@ RETURNS TRIGGER AS
 $BODY$
 BEGIN
 
-UPDATE HotelChain  
+UPDATE HotelChain
 SET number_of_hotels = (SELECT count(*)
 FROM Hotel H WHERE H.id = id);
 
@@ -21,12 +21,39 @@ $BODY$ LANGUAGE plpgsql;
 
 --Trigger that runs before a hotel instance is deleted. Calls above function.
 
-CREATE TRIGGER number_of_hotels_trigger 
+CREATE TRIGGER number_of_hotels_trigger
 AFTER UPDATE ON Hotel
 EXECUTE PROCEDURE update_number_of_hotels();
 
 --Function and Trigger 2 (NEEDS TO BE ADDED) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/**
+CREATE FUNCTION update_room_availability()
+RETURNS TRIGGER AS
+$BODY$
+BEGIN
 
+UPDATE Availability
+SET isAvailable  = FALSE
+WHERE room_number = new.room_number;
+
+RETURN NEW;
+END
+
+$BODY$ LANGUAGE plpgsql;
+
+
+--Trigger that runs after a new renting instance is inserted to update rooms availability. Calls above function.
+
+CREATE TRIGGER update_Availability
+AFTER INSERT ON Renting
+EXECUTE PROCEDURE update_room_availability()
+
+**/
+
+INSERT INTO Renting
+  ( confirmation_number, customer_id, renting_date, room_type, price, check_in_month, check_in_day, check_in_year, check_out_month, check_out_day, check_out_year, number_of_occupants, days_rented, hotel_ID, room_number, booking)
+VALUES
+  (594358315, 0, '2021-03-05', 'Single', 200, 3, 5, 2021, 3, 15, 2021, 1, 10, 0, 5, 259485837)
 
 --Query 1: Finds the minimum price of a hotel that is at least for stars
 
