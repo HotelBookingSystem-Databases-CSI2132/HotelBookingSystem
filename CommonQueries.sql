2da0307cb7ef217c5cf3837036a49e38711bb82a
Steven Li Wing <64179309+sliwi@users.noreply.github.com>
2021-04-03 12:42:20 -0400

    All Queries Completed

--- CommonQueries.sql
+++ CommonQueries.sql
@@ -30,6 +30,15 @@ Select * from renting c where c.check_in_day=10 and c.check_in_month=4
 Update customer set phone_number='456-123-7790' where id=2;
 
 ---- Query 7 -----
+SELECT RATING AS preferred_category FROM
+(SELECT rating, COUNT(hotel_id) AS hotel_num From Hotel H, BookingInfo B 
+WHERE B.hotel_id = H.id
+GROUP BY rating) AS TABLEA
+
+WHERE TABLEA.hotel_num = (SELECT MAX(hotel_num) FROM
+(SELECT rating, COUNT(hotel_id) AS hotel_num From Hotel H, BookingInfo B 
+WHERE B.hotel_id = H.id
+GROUP BY rating) AS TABLEB);
 
 ---- Query 8 -----
 SELECT Max(Salary) AS second_max_salary FROM (SELECT Salary From Employee
